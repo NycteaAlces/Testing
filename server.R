0bd6645071f60283e099dcfcd64518a882563522
Mike Russell <mike.russell@gov.ab.ca>
2017-07-14 15:54:15 -0600

    Update server.R

--- server.R
+++ server.R
@@ -36,9 +36,7 @@ shinyServer(function(input, output,session) {
 DB <- reactive(input$MegaDB$datapath)
 GIS <- reactive(input$WMU_Shp)
 
-  output$myplot <- renderPlot({
-
-    # input$file1 will be NULL initially. After the user selects and uploads a
+   # input$file1 will be NULL initially. After the user selects and uploads a
     # file, it will be a data frame with 'name', 'size', 'type', and 'datapath'
     # columns. The 'datapath' column will contain the local filenames where the
     # data can be found.
@@ -54,50 +52,58 @@ GIS <- reactive(input$WMU_Shp)
     results_num_index <- as.integer(strat_num) + 1
 
     datasheet <- as.data.frame(sqlQuery(myconn, "select * from datasheet"))
-
+    close(myconn) #close the access connection
     names(datasheet) <- sub(" ", ".", names(datasheet))
     names(datasheet) <- sub(" ", ".", names(datasheet))
     names(datasheet) <- sub("/", "", names(datasheet))
     names(datasheet) <- sub("/", "", names(datasheet))
-
-
 
     transflown <- datasheet[!duplicated(datasheet[, c("Transect.ID", "Stratum")]), ]
     transflown <- transflown[!is.na(transflown$Stratum),]
     transflown$DistancePerp <- " "
     transflown$MOOS.GroupSize <- " "
+    transflown$MUDE.GroupSize <- " "
+    transflown$WTDE.GroupSize <- " "
+    transflown$WAPT.GroupSize <- " "
     transflown$Covariate.1 <- " "
     transflown$Covariate.2 <- " "
     transflown <- unique(transflown)
 
-
-    datasheet.2 <- datasheet[ which(datasheet$MOOS.GroupSize >0),]
-    datasheet.2 <- unique(datasheet.2)
-
-
-    DistancePreInput.MOOS.2 <- anti_join(transflown, datasheet.2, by=c("Transect.ID","Stratum"))
-    DistancePreInput.MOOS.2 <- unique(DistancePreInput.MOOS.2)
-
-
-    DistancePreInput.MOOS <- merge(datasheet.2, DistancePreInput.MOOS.2, all=T)
-    DistancePreInput.MOOS <- unique(DistancePreInput.MOOS)
-
-
-    DistanceInput<- as.data.frame(cbind(object.ID = as.numeric(DistancePreInput.MOOS$ID), Region.Label= DistancePreInput.MOOS$Stratum,Area = as.numeric(DistancePreInput.MOOS$Stratum.Area), TID = as.numeric(DistancePreInput.MOOS$Transect.ID), TLENGTH = as.numeric(DistancePreInput.MOOS$Transect.Length), Effort=as.numeric(DistancePreInput.MOOS$Length)/1000, distance= as.numeric(DistancePreInput.MOOS$DistancePerp), size=as.numeric(DistancePreInput.MOOS$MOOS.GroupSize),CC=as.factor(DistancePreInput.MOOS$Covariate.1), Activity=as.factor(DistancePreInput.MOOS$Covariate.2)))
-
-    DistanceInput <- DistanceInput[ order(DistanceInput$Region.Label, DistanceInput$TID, DistanceInput$size), ]
-
-
-    close(myconn)
-
+    #Create moose table
+    datasheet.MOOS.1 <- datasheet[ which(datasheet$MOOS.GroupSize >0),]
+    datasheet.MOOS.1 <- unique(datasheet.MOOS.1)
+    #Create mule deer table
+    datasheet.MUDE.1 <- datasheet[ which(datasheet$MOOS.GroupSize >0),]
+    datasheet.MUDE.1 <- unique(datasheet.MUDE.1)
+    #Create white-tailed deer table
+    datasheet.WTDE.1 <- datasheet[ which(datasheet$WTDE.GroupSize >0),]
+    datasheet.WTDE.1 <- unique(datasheet.WTDE.1)
+    #Create elk table
+    datasheet.WAPT.1 <- datasheet[ which(datasheet$WAPT.GroupSize >0),]
+    datasheet.WAPT.1 <- unique(datasheet.WAPT.1)
+  
+  
+  getDSM <- function(SppTable, Spp_output){(
+    DistancePreInput.X <- anti_join(transflown, SppTable, by=c("Transect.ID","Stratum"))
+    DistancePreInput.X <- unique(DistancePreInput.X)
+    DistancePreInput.X.2 <- merge(SppTable, DistancePreInput.X, all=T)
+    DistancePreInput.X.2 <- unique(DistancePreInput.X)
+    DistanceInput.MOOS <- as.data.frame(cbind(object.ID = as.numeric(DistancePreInput.X.2$ID), Region.Label= DistancePreInput.X.2$Stratum,Area = as.numeric(DistancePreInput.X.2$Stratum.Area), TID = as.numeric(DistancePreInput.X.2$Transect.ID), TLENGTH = as.numeric(DistancePreInput.X.2$Transect.Length), Effort=as.numeric(DistancePreInput.X.2$Length)/1000, distance= as.numeric(DistancePreInput.X.2$DistancePerp), size=as.numeric(DistancePreInput.X.2$MOOS.GroupSize),CC=as.factor(DistancePreInput.X.2$Covariate.1), Activity=as.factor(DistancePreInput.X.2$Covariate.2)))
+    DistanceInput.MOOS <- DistanceInput[ order(DistanceInput.MOOS$Region.Label, DistanceInput.MOOS$TID, DistanceInput.MOOS$size), ]
     DistanceInput2 <- as.data.frame(cbind(object = as.numeric(DistancePreInput.MOOS$ID), Region.Label= DistancePreInput.MOOS$Stratum,Area = as.numeric(DistancePreInput.MOOS$Stratum.Area), Sample.Label = as.numeric(DistancePreInput.MOOS$Transect.ID), Effort = as.numeric(DistancePreInput.MOOS$Transect.Length), distance= as.numeric(DistancePreInput.MOOS$DistancePerp), size=as.numeric(DistancePreInput.MOOS$MOOS.GroupSize),CC=as.factor(DistancePreInput.MOOS$Covariate.1), Activity=as.factor(DistancePreInput.MOOS$Covariate.2)))
+    SppOutput <- unique(DistanceInput2)
+    })
+  
 
-    DistanceInput2 <- unique(DistanceInput2)
+   
+  
+  output$myplot <- renderPlot({
+
+     DistanceInput2 <- getDSM(
 
 
-    model1 <- ddf(method="ds", data=DistanceInput2, dsmodel = ~cds(key="hn"), meta.data=list(width=425))
-    ddf.1.moos <- ds(DistanceInput2, key="hn", adjustment = "cos", truncation = 425)
-
+    model1 <- ddf(method="ds", data=GetDSM(datasheet.MOOS.1), dsmodel = ~cds(key="hn"), meta.data=list(width=425))
+    ddf.1.moos <- ds(GetDSM(datasheet.MOOS.1), key="hn", adjustment = "cos", truncation = 425)
 
 
 
