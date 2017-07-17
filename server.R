be6c42fba14c0e24d0ba56ec588332828c11200a
Mike Russell <mike.russell@gov.ab.ca>
2017-07-17 08:59:24 -0600

    Update server.R

--- server.R
+++ server.R
@@ -34,7 +34,6 @@ shinyServer(function(input, output,session) {
 
       #Prepare the user-input slots -- dynamic/reactive
       DB <- reactive(input$MegaDB$datapath)
- print(paste(DB))
       GIS <- reactive(input$WMU_Shp)
 
    # input$file1 will be NULL initially. After the user selects and uploads a
@@ -85,13 +84,13 @@ shinyServer(function(input, output,session) {
   
   
   getDSM <- function(SppTable, SPCD){(
-    DistancePreInput.X <- anti_join(transflown, SppTable, by=c("Transect.ID","Stratum"))
-    DistancePreInput.X <- unique(DistancePreInput.X)
-    DistancePreInput.X.2 <- merge(SppTable, DistancePreInput.X, all=T)
-    DistancePreInput.X.2 <- unique(DistancePreInput.X)
-    DistanceInput.MOOS <- as.data.frame(cbind(object.ID = as.numeric(DistancePreInput.X.2$ID), Region.Label= DistancePreInput.X.2$Stratum,Area = as.numeric(DistancePreInput.X.2$Stratum.Area), TID = as.numeric(DistancePreInput.X.2$Transect.ID), TLENGTH = as.numeric(DistancePreInput.X.2$Transect.Length), Effort=as.numeric(DistancePreInput.X.2$Length)/1000, distance= as.numeric(DistancePreInput.X.2$DistancePerp), size=as.numeric(DistancePreInput.X.2$[, SPCD].GroupSize),CC=as.factor(DistancePreInput.X.2$Covariate.1), Activity=as.factor(DistancePreInput.X.2$Covariate.2)))
-    DistanceInput.MOOS <- DistanceInput[ order(DistanceInput.MOOS$Region.Label, DistanceInput.MOOS$TID, DistanceInput.MOOS$size), ]
-    DistanceInput2 <- as.data.frame(cbind(object = as.numeric(DistancePreInput.MOOS$ID), Region.Label= DistancePreInput.MOOS$Stratum,Area = as.numeric(DistancePreInput.MOOS$Stratum.Area), Sample.Label = as.numeric(DistancePreInput.MOOS$Transect.ID), Effort = as.numeric(DistancePreInput.MOOS$Transect.Length), distance= as.numeric(DistancePreInput.MOOS$DistancePerp), size=as.numeric(DistancePreInput.MOOS$[, SPCD].GroupSize),CC=as.factor(DistancePreInput.MOOS$Covariate.1), Activity=as.factor(DistancePreInput.MOOS$Covariate.2)))
+    DistancePreInput.X <- anti_join(transflown, SppTable, by=c("Transect.ID","Stratum")),
+    DistancePreInput.X <- unique(DistancePreInput.X),
+    DistancePreInput.X.2 <- merge(SppTable, DistancePreInput.X, all=T),
+    DistancePreInput.X.2 <- unique(DistancePreInput.X),
+    DistanceInput.MOOS <- as.data.frame(cbind(object.ID = as.numeric(DistancePreInput.X.2$ID), Region.Label= DistancePreInput.X.2$Stratum,Area = as.numeric(DistancePreInput.X.2$Stratum.Area), TID = as.numeric(DistancePreInput.X.2$Transect.ID), TLENGTH = as.numeric(DistancePreInput.X.2$Transect.Length), Effort=as.numeric(DistancePreInput.X.2$Length)/1000, distance= as.numeric(DistancePreInput.X.2$DistancePerp), size=as.numeric(DistancePreInput.X.2$[, SPCD].GroupSize),CC=as.factor(DistancePreInput.X.2$Covariate.1), Activity=as.factor(DistancePreInput.X.2$Covariate.2))),
+    DistanceInput.MOOS <- DistanceInput[ order(DistanceInput.MOOS$Region.Label, DistanceInput.MOOS$TID, DistanceInput.MOOS$size), ],
+    DistanceInput2 <- as.data.frame(cbind(object = as.numeric(DistancePreInput.MOOS$ID), Region.Label= DistancePreInput.MOOS$Stratum,Area = as.numeric(DistancePreInput.MOOS$Stratum.Area), Sample.Label = as.numeric(DistancePreInput.MOOS$Transect.ID), Effort = as.numeric(DistancePreInput.MOOS$Transect.Length), distance= as.numeric(DistancePreInput.MOOS$DistancePerp), size=as.numeric(DistancePreInput.MOOS$[, SPCD].GroupSize),CC=as.factor(DistancePreInput.MOOS$Covariate.1), Activity=as.factor(DistancePreInput.MOOS$Covariate.2))),
     SppOutput <- unique(DistanceInput2)
     })
      
