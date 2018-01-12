a8c491be5a0de4220a909053000f521609a246d1
Mike Russell <mike.russell@gov.ab.ca>
2018-01-12 16:11:36 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,16 +1,3 @@
-#install and load required packages -----------------
-ipak <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg))
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
-
-# usage
-packages <- c("shiny", "RODBC","dplyr","Distance","mrds", "ggplot2", "rgdal",
-              "rgeos","dsm","knitr","maptools","gridExtra","sp", "DT", "kableExtra", "ggmap",
-              "investr", "raster")
-ipak(packages)
 
 shinyUI(pageWithSidebar(
   headerPanel("Alberta Aerial Ungulate Survey Population Estimator"),
